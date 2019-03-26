803aa877ea08c2d304478632c894256f33d2b8c0
Ronan Fegan <rfegan13@gmail.com>
2019-03-26 19:44:36 +0000

    Added validation to ensure name exists within database

--- lookupPeople.js
+++ lookupPeople.js
@@ -3,16 +3,19 @@ function findPeople(client, search) {
     let counter = 1;
 
     client.query(queryString, [search], (err, res) => {
+
         if (err) {
           console.log(err);
           return;
+        } else if (!res.rows[0]){
+            console.log('Sorry. That record cannot be found.');
+            return;
         }
         console.log('Found ' + res.rows.length + ' people by the name ' + res.rows[0].first_name);
         res.rows.forEach((name) => {
             console.log(`${counter}.${name.first_name} ${name.last_name} born on: ${name.birthdate.toDateString()}`);
             counter ++;
-        });
-
+     });
   })
 }
   exports.findPeople = findPeople;
