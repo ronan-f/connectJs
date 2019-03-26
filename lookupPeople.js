40d7b50e9eeb6478ddb669ded59dc945f32afbf1
Ronan Fegan <rfegan13@gmail.com>
2019-03-26 19:12:10 +0000

    Added counter for names

--- lookupPeople.js
+++ lookupPeople.js
@@ -1,15 +1,17 @@
 function findPeople(client, search) {
     const queryString = `SELECT * FROM famous_people`;
+    let counter = 1;
 
     client.query(queryString, [], (err, res) => {
         if (err) {
           console.log(err);
           return;
         }
-
         res.rows.forEach((name) => {
+
             if(search === name.first_name){
-          console.log(`${name.first_name} ${name.last_name} born on: ${name.birthdate.toDateString()}`);
+                console.log(`${counter}.${name.first_name} ${name.last_name} born on: ${name.birthdate.toDateString()}`);
+                counter ++;
             }
         });
     });
