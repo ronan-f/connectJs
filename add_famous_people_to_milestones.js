50674c4ad855b78368bc24bded28eca5fa7abc00
Ronan Fegan <rfegan13@gmail.com>
2019-03-27 02:00:35 +0000

    Added a foregin key to milestones db

---
+++ add_famous_people_to_milestones.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+    return Promise.all([
+      knex.schema.table('milestones', function(table){
+        table.integer('famous_people_id').unsigned()
+        table.foreign('famous_people_id').references('famous_people.id')
+      })
+    ])
+  };
+
+  exports.down = function(knex, Promise) {
+    return Promise.all([
+      knex.schema.dropTable('milestones')
+    ])
+  };
